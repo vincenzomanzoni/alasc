566e257105914b81f4b37bb4dab618c78b7daee2
vincenzo.manzoni <vincenzo.manzoni@c7888f9b-3d2b-0410-8933-59234666a24e>
2007-03-12 15:46:36 +0000

    Loading of the project from local folder into SVN repository.

---
+++ src/it/unibg/alasc/examples/Attributi.java
@@ -0,0 +1,43 @@
+package it.unibg.alasc.examples;
+
+import java.util.HashMap;
+
+public class Attributi {
+	private int parziale;
+	private HashMap<String, Integer> prodotti;
+	
+	public Attributi() {
+		parziale = 0;
+		prodotti = new HashMap<String, Integer>();
+	}
+	
+	public Attributi(HashMap<String, Integer> prodotti) {
+		this();
+		this.prodotti = prodotti;
+	}
+	
+	public String toString() {
+		return "Parziale = " + parziale + ", " +
+			prodotti.toString();
+	}
+
+	public int getParziale() {
+		return parziale;
+	}
+
+	public void setParziale(int parziale) {
+		this.parziale = parziale;
+	}
+	
+	public HashMap<String, Integer> getProdotti() {
+		return prodotti;
+	}
+	
+	public void aggiornaProdotti(String nomeProdotto, int qta) {
+		if (prodotti.containsKey(nomeProdotto)) {
+			prodotti.put(nomeProdotto, prodotti.get(nomeProdotto) + qta); 
+		} else {
+			prodotti.put(nomeProdotto, qta);
+		}
+	}
+}
