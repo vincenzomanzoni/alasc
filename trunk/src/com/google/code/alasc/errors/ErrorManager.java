6f3542d4555df2b88eb077e446841d3113e3ea2e
vincenzo.manzoni <vincenzo.manzoni@c7888f9b-3d2b-0410-8933-59234666a24e>
2007-06-09 13:59:53 +0000

    Javadoc.

--- trunk/src/com/google/code/alasc/errors/ErrorManager.java
+++ trunk/src/com/google/code/alasc/errors/ErrorManager.java
@@ -4,17 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Classe per l'oggetto che gestisce le segnalazioni d'errore.
+ * Classe per che gestisce i vari tipi di errori che possono occorrere
+ * durante la compilazione.
  * 
- * @author andrea
+ * @author Andrea Rota
  *
  */
 public class ErrorManager {
 	
-	List<LexicalError> lexicalErrorList;
-	List<SemanticError> semanticErrorList;
-	List<SyntaxError> syntaxErrorList;
+	private List<LexicalError> lexicalErrorList;
+	private List<SemanticError> semanticErrorList;
+	private List<SyntaxError> syntaxErrorList;
 	
+	/**
+	 * Crea un'istanza della classe.
+	 */
 	public ErrorManager() {
 		lexicalErrorList = new ArrayList<LexicalError>();
 		semanticErrorList = new ArrayList<SemanticError>();
@@ -22,24 +26,36 @@ public class ErrorManager {
 	}
 	
 	/**
-	 * 
-	 * @param e errore da aggiungere
+	 * Aggiunge un errore lessicale.
+	 * @param e L'errore lessicale da aggiungere.
 	 */
-	//TODO Sistemare sta merda!
 	public void addError(LexicalError e) {
 		lexicalErrorList.add(e);
 	}	
 
+	/**
+	 * Aggiunge un errore sintattico.
+	 * @param e L'errore sintattico da aggiungere.
+	 */
 	public void addError(SyntaxError e) {
 		syntaxErrorList.add(e);
 	}	
+	
+	/**
+	 * Aggiunge un errore semantico.
+	 * @param e L'errore semantico.
+	 */
 	public void addError(SemanticError e) {
 		semanticErrorList.add(e);
 	}	
 	
 	/**
-	 * @return una lista di errori registrati. L'ordinamento � errori lessicali
-	 * errori sintattici ed errori semantici, successivamente cronologico.
+	 * Restituisce l'insieme degli errori che sono accorsi durante la 
+	 * compilazione.
+	 * 
+	 * @return La lista di errori; prima quelli lessicali, poi 
+	 * quelli sintattici ed infine quelli semantici. All'interno di ogni 
+	 * categoria, sono ordinati in ordine cronologico.
 	 */
 	public List<GenericError> getAllErrors(){
 		List<GenericError> listaErrori = new ArrayList<GenericError>();
@@ -56,40 +72,70 @@ public class ErrorManager {
 	}
 
 	/**
-	 * @return the lexicalErrorList
+	 * Restituisce la lista degli errori lessicali.
+	 * 
+	 * @return La lista degli errori lessicali.
 	 */
 	public List<LexicalError> getLexicalErrorList() {
 		return lexicalErrorList;
 	}
 
 	/**
-	 * @return the semanticErrorList
+	 * Restituisce la lista degli errori sintattici.
+	 * 
+	 * @return La lista degli errori sintattici.
 	 */
 	public List<SemanticError> getSemanticErrorList() {
 		return semanticErrorList;
 	}
 
 	/**
-	 * @return the syntaxErrorList
+	 * Restituisce la lista degli errori semantici.
+	 * 
+	 * @return La lista degli errori semantici.
 	 */
 	public List<SyntaxError> getSyntaxErrorList() {
 		return syntaxErrorList;
 	}
 	
+	/**
+	 * Verifica se ci sono stati errori di qualunque tipo. 
+	 * 
+	 * @return <tt>true</tt> se si sono verificati errori di qualunque tipo,
+	 * <tt>false</tt> altrimenti.
+	 */
 	public boolean hasErrors(){
 		return 	!lexicalErrorList.isEmpty() ||
 				!semanticErrorList.isEmpty() ||
 				!syntaxErrorList.isEmpty();
 	}
 	
+	/**
+	 * Verifica se ci sono stati errori lessicali.
+	 * 
+	 * @return <tt>true</tt> se si sono verificati errori lesicali, 
+	 * <tt>false</tt> altrimenti.
+	 */
 	public boolean hasLexicalErrors(){
 		return 	!lexicalErrorList.isEmpty();
 	}
 	
+	/**
+	 * Verifica se ci sono stati errori sintattici.
+	 * 
+	 * @return <tt>true</tt> se si sono verificati errori sintattici, 
+	 * <tt>false</tt> altrimenti.
+	 */
 	public boolean hasSyntaxErrors(){
 		return 	!syntaxErrorList.isEmpty();
 	}
 	
+	/**
+	 * Verifica se ci sono stati errori semantici.
+	 * 
+	 * @return <tt>true</tt> se si sono verificati errori semantici, 
+	 * <tt>false</tt> altrimenti.
+	 */
 	public boolean hasSemanticErrors(){
 		return 	!semanticErrorList.isEmpty();
 	}
