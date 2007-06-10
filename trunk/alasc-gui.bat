c91df1a312abca32762ab474398d5b18aa2ec718
vincenzo.manzoni <vincenzo.manzoni@c7888f9b-3d2b-0410-8933-59234666a24e>
2007-06-10 20:51:44 +0000

    Batch per lanciare la GUI.

---
+++ trunk/alasc-gui.bat
@@ -0,0 +1,1 @@
+java -cp bin/;lib/jargs.jar;lib/swing-layout-1.0.jar com.google.code.alasc.AlascGui 
---
+++ trunk/alasc-gui.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+java -cp bin/:lib/jargs.jar:lib/swing-layout-1.0.jar com.google.code.alasc.AlascGui
