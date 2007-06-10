78808842b74ffff9e8330dd2df6d6b25287cf684
vincenzo.manzoni <vincenzo.manzoni@c7888f9b-3d2b-0410-8933-59234666a24e>
2007-06-10 19:43:30 +0000

    Aggiunta JavaDoc

--- trunk/src/com/google/code/alasc/Beautifier.java
+++ trunk/src/com/google/code/alasc/Beautifier.java
@@ -8,9 +8,21 @@ package com.google.code.alasc;
 public abstract class Beautifier {
 	protected int tabs;
 	
+	/**
+	 * Costruttore richiamato dalle classi derivate.
+	 * 
+	 * @param tabs Il numero di tabulazioni iniziali.
+	 */
 	public Beautifier(int tabs) {
 		this.tabs = tabs;
 	}
 	
+	/**
+	 * Il metodo che le classi derivate dovranno implementare e che eseguirà
+	 * la formattazione del codice.
+	 * 
+	 * @param code Il codice da formattare.
+	 * @return Il codice formattato.
+	 */
 	public abstract String beautify(String code);	
 }
